83595bb84760f27614947c5db7b3aafbef3ca5b6
deryandreas <derytampubolon@gmail.com>
2025-09-16 15:08:29 +0700

    revisi baru tugas 3

--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-    path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<uuid:shop_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<uuid:shop_id>/', show_json_by_id, name='show_json_by_id'),
     path('add/', create_shop, name='create_shop'),
     path('detail/<uuid:shop_id>/', show_detail, name='show_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,54 +3,55 @@ from main.forms import ShopForm
 from main.models import Shop
 from django.http import HttpResponse
 from django.core import serializers
-# Create your views here.
 
+# Halaman utama
 def show_main(request):
     shop_list = Shop.objects.all()
     context = {
         "nama_apps": "Exercise sport",
         "nama_ceo": "dery andreas",
-        'umur': 'PBP D',
-        'shop_list': shop_list
+        "umur": "PBP D",
+        "shop_list": shop_list
     }
     return render(request, "main.html", context)
 
+# Form tambah shop
 def create_shop(request):
     if request.method == "POST":
         form = ShopForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('main:show_main')
+            return redirect("main:show_main")
     else:
         form = ShopForm()
-        return render(request, "create_shop.html", {"form": form})
+    return render(request, "create_shop.html", {"form": form})
 
+# Detail shop (pakai UUID)
 def show_detail(request, shop_id):
     shop_item = get_object_or_404(Shop, pk=shop_id)
+    shop_item.increment_views()  # contoh: nambah views
     return render(request, "detail_shop.html", {"shop_item": shop_item})
 
+# Semua data XML
 def show_xml(request):
-     shop_list = Shop.objects.all()
-     xml_data = serializers.serialize("xml", shop_list)
-     return HttpResponse(xml_data, content_type="application/xml")
+    shop_list = Shop.objects.all()
+    xml_data = serializers.serialize("xml", shop_list)
+    return HttpResponse(xml_data, content_type="application/xml")
 
+# Semua data JSON
 def show_json(request):
     shop_list = Shop.objects.all()
     json_data = serializers.serialize("json", shop_list)
     return HttpResponse(json_data, content_type="application/json")
 
+# Data berdasarkan UUID (XML)
 def show_xml_by_id(request, shop_id):
-   try:
-       shop_item = Shop.objects.filter(pk=shop_id)
-       xml_data = serializers.serialize("xml", shop_item)
-       return HttpResponse(xml_data, content_type="application/xml")
-   except Shop.DoesNotExist:
-       return HttpResponse(status=404)
+    shop_item = get_object_or_404(Shop, pk=shop_id)
+    xml_data = serializers.serialize("xml", [shop_item])
+    return HttpResponse(xml_data, content_type="application/xml")
 
+# Data berdasarkan UUID (JSON)
 def show_json_by_id(request, shop_id):
-   try:
-       shop_item = Shop.objects.get(pk=shop_id)
-       json_data = serializers.serialize("json", [shop_item])
-       return HttpResponse(json_data, content_type="application/json")
-   except Shop.DoesNotExist:
-       return HttpResponse(status=404)
+    shop_item = get_object_or_404(Shop, pk=shop_id)
+    json_data = serializers.serialize("json", [shop_item])
+    return HttpResponse(json_data, content_type="application/json")
